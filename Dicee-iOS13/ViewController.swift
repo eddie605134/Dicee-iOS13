a5b19edf3fe2b443ea9d9bbaf4181aee68193554
eddie <eddie605134@gmailcom>
2024-11-12 16:31:22 +0800

    complete class

--- Dicee-iOS13/ViewController.swift
+++ Dicee-iOS13/ViewController.swift
@@ -9,12 +9,27 @@
 import UIKit
 
 class ViewController: UIViewController {
+	
+	@IBOutlet weak var diceImageViewLeft: UIImageView!
+	@IBOutlet weak var diceImageViewRight: UIImageView!
+	
+	// set a array one to six
+	let diceArray: [String] = ["DiceOne", "DiceTwo", "DiceThree", "DiceFour", "DiceFive", "DiceSix"]
 
-    override func viewDidLoad() {
-        super.viewDidLoad()
-        // Do any additional setup after loading the view.
-    }
+	override func viewDidLoad() {
+		super.viewDidLoad()
+		diceImageViewLeft.image = UIImage(named: "DiceOne")
+		diceImageViewRight.image = UIImage(named: "DiceTwo")
+	}
 
-
+	@IBAction func pressRollBtn(_ sender: Any) {
+		// random show dice image One~Six
+		let randomIndexLeft = Int.random(in: 0..<6)
+		let randomIndexRight = Int.random(in: 0..<6)
+		
+		diceImageViewLeft.image = UIImage(named: diceArray[randomIndexLeft])
+		diceImageViewRight.image = UIImage(named: diceArray[randomIndexRight])
+	}
+	
 }
 
